13d4a64f0d3f0e0ac44b2d5acc8dba37f0d4093d
Radon Rosborough <radon.neon@gmail.com>
2020-03-23 22:21:24 -0600

    Add package commentary

--- ctrlf.el
+++ ctrlf.el
@@ -11,6 +11,14 @@
 
 ;;; Commentary:
 
+;; CTRLF (pronounced "control F") is an intuitive and efficient
+;; solution for single-buffer text search in Emacs, replacing packages
+;; such as Isearch, Swiper, and helm-swoop. Taking inspiration from
+;; the widely-adopted and battle-tested ctrl+F interfaces in programs
+;; such as web browsers, but following the flow and keybindings of
+;; Isearch, CTRLF improves on existing text search solutions in
+;; convenience, robustness, and consistency.
+
 ;; Please see https://github.com/raxod502/ctrlf for more
 ;; information.
 
